c556ecfcb5e50be4ba182fd43b834c2eb42777b3
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-03-17 14:14:30 +0000

    creation du package web et le controlleur PatientController et la page patients.html

---
+++ src/main/java/ma/enset/hospital_system/web/PatientController.java
@@ -0,0 +1,24 @@
+package ma.enset.hospital_system.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.hospital_system.entities.Patient;
+import ma.enset.hospital_system.repository.PatientRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+
+    private PatientRepository patientRepository;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("patients", patients);
+        return "patients";
+    }
+}
