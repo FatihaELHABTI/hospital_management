6a3ec9c5b07311c2cfa5c43253ad5c85389a6943
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-03-17 16:07:21 +0000

    ajouter la partie de la pagination

--- src/main/java/ma/enset/hospital_system/web/PatientController.java
+++ src/main/java/ma/enset/hospital_system/web/PatientController.java
@@ -3,6 +3,8 @@ package ma.enset.hospital_system.web;
 import lombok.AllArgsConstructor;
 import ma.enset.hospital_system.entities.Patient;
 import ma.enset.hospital_system.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +18,9 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model, int page, int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients", pagePatients.getContent());
         return "patients";
     }
 }
