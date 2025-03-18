da71f6532369a125b475f4e0c083a4fbea25ffe4
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-03-18 11:26:52 +0000

    partie pagination terminee , et la partie de rechercher avec un mot cle

--- src/main/java/ma/enset/hospital_system/repository/PatientRepository.java
+++ src/main/java/ma/enset/hospital_system/repository/PatientRepository.java
@@ -1,7 +1,11 @@
 package ma.enset.hospital_system.repository;
 
 import ma.enset.hospital_system.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository  extends JpaRepository<Patient, Integer> {
+
+    Page<Patient> findByNomContains(String keyword , Pageable pageable);
 }
--- src/main/java/ma/enset/hospital_system/web/PatientController.java
+++ src/main/java/ma/enset/hospital_system/web/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +19,16 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model, int page, int size) {
-        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "4")  int size,
+                        @RequestParam(name = "keyword",defaultValue = "")  String keyword) {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
+
         model.addAttribute("patients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
         return "patients";
     }
 }
