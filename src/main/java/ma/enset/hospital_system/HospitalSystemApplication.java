1383fe363c3b475e64c0fc49b56b291791b8dd29
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-04-13 20:35:14 +0100

    securityConfig class and inMemoryUserDetailsManager method added

--- src/main/java/ma/enset/hospital_system/HospitalSystemApplication.java
+++ src/main/java/ma/enset/hospital_system/HospitalSystemApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -44,5 +47,10 @@ public class HospitalSystemApplication  implements CommandLineRunner {
 //        patientRepository.save(new Patient(null,"Najat",new Date(),false,89));
 //        patientRepository.save(new Patient(null,"Siham",new Date(),false,34));
 
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
